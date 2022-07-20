71e3d188fb55bd613ba0d2c84e06a1d58d9a7c5c
himashuBisht <30807383+himashuBisht@users.noreply.github.com>
2022-07-20 10:09:08 +0530

    Create plates.py

---
+++ pset2/plates.py
@@ -0,0 +1,34 @@
+def main():
+    plate = input("Plate: ")
+    if is_valid(plate):
+        print("Valid")
+    else:
+        print("Invalid")
+
+
+def is_valid(num):
+    # “All vanity plates must start with at least two letters.”
+    # For example, AAA222 would be an acceptable …vanity plate; AAA22A would not be acceptable.
+    num = list(num)
+    c = 0
+    # vanity plates may contain a maximum of 6 characters (letters or numbers) and a minimum of 2 characters.”
+    if 3 > len(num) or len(num) > 6:
+        return False
+    for n in num:
+        if n in [",", ".", " "]:
+            return False
+        # The first number used cannot be a ‘0’.
+        if n.isdigit():
+            c += 1
+            if c == 1 and n == "0":
+                return False
+    # “Numbers cannot be used in the middle of a plate; they must come at the end.
+    # cs50p2 is invalid
+    if num[len(num)-1].isdigit() and num[len(num)-2].isalpha():
+        return False
+
+    else:
+        return True
+
+
+main()
